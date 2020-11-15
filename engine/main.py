25b75b29b3df5493d9a9e08d1aa70fe12db43de6
bgroman <benp.groman@gmail.com>
2020-11-15 01:51:24 -0500

    Static venue endpoint with one venue.

--- engine/main.py
+++ engine/main.py
@@ -14,3 +14,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/venues")
+def read_venues():
+    return [{"id": "PDC", "name": "Pathfinder Dining Center", "hours": {"today": {"Start": "T11:30", "End": "T15"}, "tomorrow": {"Start": "T10", "End": "T14"}}}]
